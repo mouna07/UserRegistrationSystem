83581a484bf98117ac37bb6b518bc43d2d256f70
mouna07 <mounamc261@gmail.com>
2021-02-18 00:07:58 +0530

    ADDED: validation of Email Samples

--- src/main/java/com/BridgeLabz/UserRegistration.java
+++ src/main/java/com/BridgeLabz/UserRegistration.java
@@ -6,47 +6,44 @@ import java.util.regex.Pattern;
 public class UserRegistration {
     public static void main(String args[]) {
             UserRegistration User = new UserRegistration();
-
+            Scanner scanner = new Scanner(System.in);
             System.out.println("**********welcome to the user registration problem**********");
             System.out.println("enter the first name:");
-            User.ValidateName();
+            User.ValidateName(scanner.next());
             System.out.println("enter the Last name:");
-            User.ValidateName();
+            User.ValidateName(scanner.next());
             System.out.println("Enter the valid email id:");
-            User.ValidateEmail();
+            User.ValidateEmail(scanner.next());
             System.out.println("Enter the valid phone number:");
-            User.ValidatePhoneNo();
+            User.ValidatePhoneNo(scanner.next());
     }
-    public void ValidateName(){
-        Scanner sc = new Scanner(System.in);
-        String name = sc.next();
+    public void ValidateName( String name){
+
         if (Pattern.matches("[A-Z][a-z]{2}[a-z]*", name)) {
             System.out.println("Valid  Name");
         } else {
             System.out.println("Invalid  Name");
         }
     }
-    public void ValidateEmail(){
-        Scanner sc = new Scanner(System.in);
-        String Email = sc.next();
-        if (Pattern.matches("[a-zA-Z]+([._+-]{0,1}[a-zA-Z0-9]+)*@[a-zA-Z0-9]+.[a-zA-Z]{2,4}+(?:\\\\.[a-z]{2,}){0,1}$)", Email)) {
+    public String ValidateEmail(String email){
+
+        if (Pattern.matches("^[a-zA-Z0-9_+&*-]+(?:\\\\.\" + \"[a-zA-Z0-9_+&*-]+)*@\" + \"(?:[a-zA-Z0-9-]+\\\\.)+[a-z\" + \"A-Z]{2,7}$", email)) {
             System.out.println("Valid EmailId");
         } else {
             System.out.println("Invalid  EmailId");
         }
+        return email;
     }
-    public void ValidatePhoneNo(){
-        Scanner sc = new Scanner(System.in);
-        String Number = sc.next();
+    public void ValidatePhoneNo(String Number){
+
         if (Pattern.matches("(([0-9]{2})?)[ ][0-9]{10}", Number)) {
             System.out.println("Valid Phone Number");
         } else {
             System.out.println("Invalid Phone Number");
         }
     }
-    public void ValidatePassword(){
-        Scanner sc = new Scanner(System.in);
-        String password = sc.next();
+    public void ValidatePassword(String password){
+
         if (Pattern.matches("((^(?=.*[A-Z]))(?=.*[0-9])(?=.*[a-z])(?=.*[@*&^%#-*+!]{1}).{8,}$)", password)) {
             System.out.println("Valid Password");
         } else {
