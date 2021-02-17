0f87557a9d9252f5addec0c4697244bef5600563
mouna07 <mounamc261@gmail.com>
2021-02-17 22:46:35 +0530

    ADDED: Welcome Message

---
+++ src/main/java/com/BridgeLabz/UserRegistration.java
@@ -0,0 +1,7 @@
+package com.BridgeLabz;
+
+public class UserRegistration {
+    public static void main(String args[]) {
+            System.out.println("**********welcome to the user registration problem**********");
+    }
+}
