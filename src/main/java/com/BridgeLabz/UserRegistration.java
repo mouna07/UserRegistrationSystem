da05fabd15f3739e084590907dded1bae1b434b4
mouna07 <mounamc261@gmail.com>
2021-02-17 22:56:59 +0530

    ADDED: validation of first name

--- src/main/java/com/BridgeLabz/UserRegistration.java
+++ src/main/java/com/BridgeLabz/UserRegistration.java
@@ -1,7 +1,23 @@
 package com.BridgeLabz;
 
+import java.util.Scanner;
+import java.util.regex.Pattern;
+
 public class UserRegistration {
     public static void main(String args[]) {
+            UserRegistration User = new UserRegistration();
+
             System.out.println("**********welcome to the user registration problem**********");
+            System.out.println("enter the first name");
+            User.ValidateName();
+    }
+    public void ValidateName(){
+        Scanner sc = new Scanner(System.in);
+        String name = sc.next();
+        if (Pattern.matches("[A-Z][a-z]{2}[a-z]*", name)) {
+            System.out.println("Valid  Name");
+        } else {
+            System.out.println("Invalid  Name");
+        }
     }
 }
