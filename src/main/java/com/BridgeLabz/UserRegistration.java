371e7006cfe52df5f2ca945404f2a9f19591c8ab
mouna07 <mounamc261@gmail.com>
2021-02-18 00:13:10 +0530

    ADDED: validation of Email Samples

--- src/main/java/com/BridgeLabz/UserRegistration.java
+++ src/main/java/com/BridgeLabz/UserRegistration.java
@@ -1,54 +1,103 @@
 package com.BridgeLabz;
 
 import java.util.Scanner;
+import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class UserRegistration {
+
+
+    public static boolean firstName() {
+        System.out.println("Enter first Name");
+        Scanner input = new Scanner(System.in);
+        String entry = input.nextLine();
+        Pattern pattern = Pattern.compile("[A-Z][a-z]{2}[a-z]*");
+        Matcher match = pattern.matcher(entry);
+        boolean patternValidity = match.matches();
+        if(patternValidity == true) {
+            System.out.println("happy");
+        }
+        else {
+            System.out.println("sad");
+        }
+        return patternValidity;
+    }
+
+    public static boolean lastName() {
+        System.out.println("Enter last Name");
+        Scanner input = new Scanner(System.in);
+        String entry = input.nextLine();
+        Pattern pattern = Pattern.compile("[A-Z][a-z]{2}[a-z]*");
+        Matcher match = pattern.matcher(entry);
+        boolean patternValidity = match.matches();
+        if(patternValidity == true) {
+            System.out.println("happy");
+        }
+        else {
+            System.out.println("sad");
+        }
+        return patternValidity;
+    }
+
+    public static boolean email() {
+        System.out.println("Enter your Email");
+        Scanner input = new Scanner(System.in);
+        String email = input.nextLine();
+
+
+        Pattern pattern = Pattern.compile("^[a-zA-Z0-9_+&-]+(?:\\." + "[a-zA-Z0-9_+&-]+)*@" + "(?:[a-zA-Z0-9-]+\\.)+[a-z" + "A-Z]{2,7}$");
+
+        Matcher match = pattern.matcher(email);
+        boolean patternValidity = match.matches();
+        if(patternValidity == true) {
+            System.out.println("happy");
+        }
+        else {
+            System.out.println("sad");
+        }
+        return patternValidity;
+
+    }
+
+    public static boolean mobileNumber() {
+        System.out.println("Enter Valid Mobile Number");
+        Scanner input = new Scanner(System.in);
+        String mobileNumber = input.nextLine();
+        Pattern pattern = Pattern.compile("^[1-9]{2}[ ]{1}[1-9]{1}[0-9]{9}$");
+        Matcher match = pattern.matcher(mobileNumber);
+        boolean patternValidity = match.matches();
+        if(patternValidity == true) {
+            System.out.println("happy");
+        }
+        else {
+            System.out.println("sad");
+        }
+        return patternValidity;
+    }
+
+
+    public static boolean validatePassword() {
+        System.out.println("Enter password it should have atleast one Uppercase, one digit and one special character");
+        Scanner input = new Scanner(System.in);
+        String password = input.nextLine();
+
+
+        Pattern pattern = Pattern.compile("^(?=.[a-z])(?=.[A-Z])(?=.[0-9])(?=.[!@#$%^&])[a-zA-Z0-9!@#$%^&]{8,}");
+        Matcher match = pattern.matcher(password);
+        boolean patternValidity = match.matches();
+        if(patternValidity == true) {
+            System.out.println("happy");
+        }
+        else {
+            System.out.println("sad");
+        }
+        return patternValidity;
+    }
     public static void main(String args[]) {
-            UserRegistration User = new UserRegistration();
-            Scanner scanner = new Scanner(System.in);
-            System.out.println("**********welcome to the user registration problem**********");
-            System.out.println("enter the first name:");
-            User.ValidateName(scanner.next());
-            System.out.println("enter the Last name:");
-            User.ValidateName(scanner.next());
-            System.out.println("Enter the valid email id:");
-            User.ValidateEmail(scanner.next());
-            System.out.println("Enter the valid phone number:");
-            User.ValidatePhoneNo(scanner.next());
+        firstName();
+        lastName();
+        email();
+        mobileNumber();
+        validatePassword();
     }
-    public void ValidateName( String name){
-
-        if (Pattern.matches("[A-Z][a-z]{2}[a-z]*", name)) {
-            System.out.println("Valid  Name");
-        } else {
-            System.out.println("Invalid  Name");
-        }
-    }
-    public String ValidateEmail(String email){
-
-        if (Pattern.matches("^[a-zA-Z0-9_+&*-]+(?:\\\\.\" + \"[a-zA-Z0-9_+&*-]+)*@\" + \"(?:[a-zA-Z0-9-]+\\\\.)+[a-z\" + \"A-Z]{2,7}$", email)) {
-            System.out.println("Valid EmailId");
-        } else {
-            System.out.println("Invalid  EmailId");
-        }
-        return email;
-    }
-    public void ValidatePhoneNo(String Number){
-
-        if (Pattern.matches("(([0-9]{2})?)[ ][0-9]{10}", Number)) {
-            System.out.println("Valid Phone Number");
-        } else {
-            System.out.println("Invalid Phone Number");
-        }
-    }
-    public void ValidatePassword(String password){
-
-        if (Pattern.matches("((^(?=.*[A-Z]))(?=.*[0-9])(?=.*[a-z])(?=.*[@*&^%#-*+!]{1}).{8,}$)", password)) {
-            System.out.println("Valid Password");
-        } else {
-            System.out.println("Invalid Password");
-        }
-    }
-
 }
--- src/test/java/com/BridgeLabz/UserRegistrationTest.java
+++ src/test/java/com/BridgeLabz/UserRegistrationTest.java
@@ -1,64 +1,44 @@
 package com.BridgeLabz;
 
+
 import org.junit.Assert;
 import org.junit.Test;
-import static org.junit.Assert.*;
-import java.util.Scanner;
 
-public class UserRegistrationTest {
-    UserRegistration userRegistration = new UserRegistration();
+import static org.junit.Assert.assertEquals;
+
+public class UserRegistrationTest
+{
+
     @Test
-    public void validateEmail(){
-        String testValidString1 = userRegistration.ValidateEmail("abc@yahoo.com");
-        String testValidString2 = userRegistration.ValidateEmail("abc-100@yahoo.com");
-        String testValidString3 = userRegistration.ValidateEmail("abc.100@yahoo.com");
-        String testValidString4 = userRegistration.ValidateEmail("abc111@abc.com");
-        String testValidString5 = userRegistration.ValidateEmail("abc-100@abc.net");
-        String testValidString6 = userRegistration.ValidateEmail("abc.100@abc.com.au");
-        String testValidString7 = userRegistration.ValidateEmail("abc@1.com");
-        String testValidString8 = userRegistration.ValidateEmail("abc@gmail.com.com");
-        String testValidString9 = userRegistration.ValidateEmail("abc+100@gmail.com");
+    public void test() {
+        boolean result = UserRegistration.firstName();
+        assertEquals(true, result);
 
-        String testInvalidString1 = userRegistration.ValidateEmail("abc");
-        String testInvalidString2 = userRegistration.ValidateEmail("abc@.com.my");
-        String testInvalidString3 = userRegistration.ValidateEmail("abc123@gmail.a");
-        String testInvalidString4 = userRegistration.ValidateEmail("abc123@.com");
-        String testInvalidString5 = userRegistration.ValidateEmail("abc123@.com.com");
-        String testInvalidString6 = userRegistration.ValidateEmail(".abc@abc.com");
-        String testInvalidString7 = userRegistration.ValidateEmail("abc()*@gmail.com");
-        String testInvalidString8 = userRegistration.ValidateEmail("abc@%*.com");
-        String testInvalidString9 = userRegistration.ValidateEmail("abc..2002@gmail.com");
-        String testInvalidString10 = userRegistration.ValidateEmail("abc.@gmail.com");
-        String testInvalidString11 = userRegistration.ValidateEmail("abc@abc@gmail.com");
-        String testInvalidString12 = userRegistration.ValidateEmail("abc@gmail.com.1a");
+    }
+    @Test
+    public void test1() {
+        boolean result = UserRegistration.lastName();
+        assertEquals(true, result);
 
-        Assert.assertEquals("InValid",testValidString1);
-        Assert.assertEquals("Valid",testValidString2);
-        Assert.assertEquals("Valid",testValidString3);
-        Assert.assertEquals("Valid",testValidString4);
-        Assert.assertEquals("Valid",testValidString5);
-        Assert.assertEquals("Valid",testValidString6);
-        Assert.assertEquals("Valid",testValidString7);
-        Assert.assertEquals("Valid",testValidString8);
-        Assert.assertEquals("Valid",testValidString9);
+    }
+    @Test
+    public void test2() {
+        boolean result = UserRegistration.email();
+        assertEquals(true, result);
 
-        Assert.assertEquals("Invalid",testInvalidString1);
-        Assert.assertEquals("Invalid",testInvalidString2);
-        Assert.assertEquals("Invalid",testInvalidString2);
-        Assert.assertEquals("Invalid",testInvalidString3);
-        Assert.assertEquals("Invalid",testInvalidString4);
-        Assert.assertEquals("Invalid",testInvalidString5);
-        Assert.assertEquals("Invalid",testInvalidString6);
-        Assert.assertEquals("Invalid",testInvalidString7);
-        Assert.assertEquals("Invalid",testInvalidString8);
-        Assert.assertEquals("Invalid",testInvalidString9);
-        Assert.assertEquals("Invalid",testInvalidString10);
-        Assert.assertEquals("Invalid",testInvalidString11);
-        Assert.assertEquals("Invalid",testInvalidString12);
+    }
+    @Test
+    public void test3() {
+        boolean result = UserRegistration.mobileNumber();
+        assertEquals(true, result);
+
+    }
+    @Test
+    public void test4() {
+        boolean result = UserRegistration.validatePassword();
+        assertEquals(true, result);
+
     }
 
 
-
 }
-
-
