2d037d084baf5d8a4ed1c94990ae5a46bfe9353c
mouna07 <mounamc261@gmail.com>
2021-02-18 01:07:32 +0530

    ADDED: validation of Email Samples

--- src/main/java/com/BridgeLabz/UserRegistration.java
+++ src/main/java/com/BridgeLabz/UserRegistration.java
@@ -1,72 +1,102 @@
 package com.BridgeLabz;
 
-import java.util.regex.Matcher;
+import java.util.Scanner;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class UserRegistration {
 
-    public static String UC1(String firstNameUserInput) {
+    public static void main(String args[]) throws UserRegistrationException {
+        UserRegistration User = new UserRegistration();
+        Scanner sc =new Scanner(System.in);
+        System.out.println("enter the first name");
+        User.UC1(sc.nextLine());
+        System.out.println("enter the last name");
+        User.UC2(sc.nextLine());
+        System.out.println("enter the email name");
+        User.UC3(sc.nextLine());
+        System.out.println("enter the phone name");
+        User.UC4(sc.nextLine());
+        System.out.println("enter the password name");
+        User.UC5toUC8(sc.nextLine());
+    }
+
+    public  String UC1(String firstNameUserInput) throws UserRegistrationException {
         Pattern firstName = Pattern.compile("[A-Z]{1}[a-z]{2,}$");
         Matcher match = firstName.matcher(firstNameUserInput);
         boolean patternValidity = match.matches();
-        if (patternValidity == true) {
-            return "HAPPY";
+        try {
+            if (patternValidity == true)
+                return "HAPPY";
+            else
+                return "SAD";
         }
-        else {
-            return "SAD";
+        catch(NullPointerException e) {
+            throw new UserRegistrationException("Please enter valid first name");
         }
     }
 
-    public static String UC2(String lastNameUserInput) {
+    public  String UC2(String lastNameUserInput) throws UserRegistrationException {
         Pattern lastName = Pattern.compile("[A-Z]{1}[a-z]{2,}$");
         Matcher match = lastName.matcher(lastNameUserInput);
         boolean patternValidity = match.matches();
-        if (patternValidity == true) {
-            return "HAPPY";
+        try {
+            if (patternValidity == true)
+                return "HAPPY";
+            else
+                return "SAD";
         }
-        else {
-            return "SAD";
+        catch(NullPointerException e) {
+            throw new UserRegistrationException("Please enter valid last name");
         }
     }
 
-    public static String UC3(String emailUserInput) {
+    public  String UC3(String emailUserInput) throws UserRegistrationException {
         Pattern email = Pattern.compile("^[a-zA-Z]{3}\\.[a-zA-Z]{1,}@[a-zA-Z]{2}\\.[a-zA-Z]{2}\\.[a-zA-Z]{1,}$");
         Matcher match = email.matcher(emailUserInput);
         boolean patternValidity = match.matches();
-        if (patternValidity == true) {
-            return "HAPPY";
+        try {
+            if (patternValidity == true)
+                return "HAPPY";
+            else
+                return "SAD";
         }
-        else {
-            return "SAD";
+        catch(NullPointerException e) {
+            throw new UserRegistrationException("Please enter valid Email");
         }
     }
 
-    public String UC4(String mobileUserInput) {
+    public String UC4(String mobileUserInput) throws UserRegistrationException {
         Pattern mobileNumber = Pattern.compile("^[1-9]{2}\\s{1}[0-9]{1}[0-9]{9}$");
         Matcher match = mobileNumber.matcher(mobileUserInput);
         boolean patternValidity = match.matches();
-        if (patternValidity == true) {
-            return "HAPPY";
+        try {
+            if (patternValidity == true)
+                return "HAPPY";
+            else
+                return "SAD";
         }
-        else {
-            return "SAD";
+        catch(NullPointerException e) {
+            throw new UserRegistrationException("Please enter valid mobile number");
         }
     }
 
-    public static String UC5toUC8(String passwordUserInput) {
+    public  String UC5toUC8(String passwordUserInput) throws UserRegistrationException {
         Pattern password = Pattern.compile("^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#$%^&*])[a-zA-Z0-9!@#$%^&*]{8,}");
         Matcher match = password.matcher(passwordUserInput);
         boolean patternValidity = match.matches();
-        if (patternValidity == true) {
-            return "HAPPY";
+        try {
+            if (patternValidity == true)
+                return "HAPPY";
+            else
+                return "SAD";
         }
-        else {
-            return "SAD";
+        catch(NullPointerException e) {
+            throw new UserRegistrationException("Please enter valid password");
         }
     }
 
-    public static boolean UC9(String sampleEmailValidation) {
+    public  boolean UC9(String sampleEmailValidation) {
         Pattern emailValidation = Pattern.compile("^([a-zA-a0-9\\.\\-\\+]+)@([a-zA-Z0-9\\.]{1,5})([a-zA-Z\\.]+){1,3}([a-zA-Z]{1,3})$");
         Matcher match = emailValidation.matcher(sampleEmailValidation);
         boolean patternValidity = match.matches();
--- src/test/java/com/BridgeLabz/UserRegistrationTest.java
+++ src/test/java/com/BridgeLabz/UserRegistrationTest.java
@@ -5,77 +5,109 @@ import org.junit.Test;
 import static org.junit.Assert.*;
 import java.util.Scanner;
 
-public class UserRegistrationGradleTest {
+class UserRegistrationeTest {
 
     static String Happy = null;
 
-    public static void UC1() {
-        UserRegistration userRegistration = new UserRegistration();
-        String firstNameUserInput = userRegistration.UC1("Komal");
-        //Assert Statement
-        assertEquals(Happy, firstNameUserInput);
+    public void UC1() throws UserRegistrationException {
+        UserRegistration userRegistrationGradle = new UserRegistration();
+        String firstNameUserInput = userRegistrationGradle.UC1("Komal");
+        try {
+            userRegistrationGradle.UC1(Happy);
+        } catch (UserRegistrationException e) {
+            //Assert Statement
+            assertEquals("Please enter valid first name", e.getMessage());
+        }
     }
 
-    public static void UC2() {
-        UserRegistration userRegistration = new UserRegistration();
-        String lastNameUserInput = userRegistration.UC2("Shinde");
-        //Assert Statement
-        assertEquals(Happy, "lastNameUserInput");
+    public static void UC2() throws UserRegistrationException {
+        UserRegistration userRegistrationGradle = new UserRegistration();
+        String lastNameUserInput = userRegistrationGradle.UC2("Shinde");
+        try {
+            userRegistrationGradle.UC2(Happy);
+        } catch (UserRegistrationException e) {
+            //Assert Statement
+            assertEquals("Please enter valid last name", e.getMessage());
+        }
     }
 
-    public static void UC3() {
-        UserRegistration userRegistration = new UserRegistration();
-        String emailUserInput = userRegistration.UC3("abc.xyz@bl.co.in");
-        //Assert Statement
-        assertEquals(Happy, "emailUserInput");
+    public static void UC3() throws UserRegistrationException {
+        UserRegistration userRegistrationGradle = new UserRegistration();
+        String emailUserInput = userRegistrationGradle.UC3("abc.xyz@bl.co.in");
+        try {
+            userRegistrationGradle.UC3(Happy);
+        } catch (UserRegistrationException e) {
+            //Assert Statement
+            assertEquals("Please enter valid email", e.getMessage());
+        }
     }
 
-    public static void UC4() {
-        UserRegistration userRegistration = new UserRegistration();
-        String mobileUserInput = userRegistration.UC4("91 8562416354");
-        //Assert Statement
-        assertEquals(Happy, "mobileUserInput");
+    public static void UC4() throws UserRegistrationException {
+        UserRegistration userRegistrationGradle = new UserRegistration();
+        String mobileUserInput = userRegistrationGradle.UC4("91 8562416354");
+        try {
+            userRegistrationGradle.UC4(Happy);
+        } catch (UserRegistrationException e) {
+            //Assert Statement
+            assertEquals("Please enter valid mobile number", e.getMessage());
+        }
     }
 
-    public static void UC5PasswordRule1() {
-        UserRegistration userRegistration = new UserRegistration();
-        String passwordUserInput = userRegistration.UC5toUC8("Komal");
-        //Assert Statement
-        assertEquals(Happy, "passwordUserInput");
+    public static void UC5PasswordRule1() throws UserRegistrationException {
+        UserRegistration userRegistrationGradle = new UserRegistration();
+        String passwordUserInput = userRegistrationGradle.UC5toUC8("Komal");
+        try {
+            userRegistrationGradle.UC5toUC8(Happy);
+        } catch (UserRegistrationException e) {
+            //Assert Statement
+            assertEquals("Please enter valid password", e.getMessage());
+        }
     }
 
-    public static void UC6PasswordRule2() {
-        UserRegistration userRegistration = new UserRegistration();
-        String passwordUserInput = userRegistration.UC5toUC8("Komal");
-        //Assert Statement
-        assertEquals(Happy, "passwordUserInput");
+    public static void UC6PasswordRule2() throws UserRegistrationException {
+        UserRegistration userRegistrationGradle = new UserRegistration();
+        String passwordUserInput = userRegistrationGradle.UC5toUC8("Komal");
+        try {
+            userRegistrationGradle.UC5toUC8(Happy);
+        } catch (UserRegistrationException e) {
+            //Assert Statement
+            assertEquals("Please enter valid password", e.getMessage());
+        }
     }
 
-    public static void UC7PasswordRule3() {
-        UserRegistration userRegistration = new UserRegistration();
-        String passwordUserInput = userRegistration.UC5toUC8("Komal");
-        //Assert Statement
-        assertEquals(Happy, "passwordUserInput");
+    public static void UC7PasswordRule3() throws UserRegistrationException {
+        UserRegistration userRegistrationGradle = new UserRegistration();
+        String passwordUserInput = userRegistrationGradle.UC5toUC8("Komal");
+        try {
+            userRegistrationGradle.UC5toUC8(Happy);
+        } catch (UserRegistrationException e) {
+            //Assert Statement
+            assertEquals("Please enter valid password", e.getMessage());
+        }
     }
 
-    public static void UC8PasswordRule4() {
-        UserRegistration userRegistration = new UserRegistration();
-        String passwordUserInput = userRegistration.UC5toUC8("Komal");
-        //Assert Statement
-        assertEquals(Happy, "passwordUserInput");
+    public static void UC8PasswordRule4() throws UserRegistrationException {
+        UserRegistration userRegistrationGradle = new UserRegistration();
+        String passwordUserInput = userRegistrationGradle.UC5toUC8("Komal");
+        try {
+            userRegistrationGradle.UC5toUC8(Happy);
+        } catch (UserRegistrationException e) {
+            //Assert Statement
+            assertEquals("Please enter valid password", e.getMessage());
+        }
     }
 
     public static void UC9() {
-        UserRegistration userRegistration = new UserRegistration();
-        boolean sampleEmailValidation1 = userRegistration.UC9("abc@yahoo.com");
-        boolean sampleEmailValidation2 = userRegistration.UC9("abc-100@yahoo.com");
-        boolean sampleEmailValidation3 = userRegistration.UC9("abc.100@yahoo.com");
-        boolean sampleEmailValidation4 = userRegistration.UC9("abc111@abc.com");
-        boolean sampleEmailValidation5 = userRegistration.UC9("abc-100@abc.net");
-        boolean sampleEmailValidation6 = userRegistration.UC9("abc.100@abc.com.au");
-        boolean sampleEmailValidation7 = userRegistration.UC9("abc@1.com");
-        boolean sampleEmailValidation8 = userRegistration.UC9("abc@gmail.com.com");
-        boolean sampleEmailValidation9 = userRegistration.UC9("abc+100@gmail.com");
+        UserRegistration userRegistrationGradle = new UserRegistration();
+        boolean sampleEmailValidation1 = userRegistrationGradle.UC9("abc@yahoo.com");
+        boolean sampleEmailValidation2 = userRegistrationGradle.UC9("abc-100@yahoo.com");
+        boolean sampleEmailValidation3 = userRegistrationGradle.UC9("abc.100@yahoo.com");
+        boolean sampleEmailValidation4 = userRegistrationGradle.UC9("abc111@abc.com");
+        boolean sampleEmailValidation5 = userRegistrationGradle.UC9("abc-100@abc.net");
+        boolean sampleEmailValidation6 = userRegistrationGradle.UC9("abc.100@abc.com.au");
+        boolean sampleEmailValidation7 = userRegistrationGradle.UC9("abc@1.com");
+        boolean sampleEmailValidation8 = userRegistrationGradle.UC9("abc@gmail.com.com");
+        boolean sampleEmailValidation9 = userRegistrationGradle.UC9("abc+100@gmail.com");
         //Assert Statement
         assertEquals(true, "sampleEmailValidation1");
         assertEquals(true, "sampleEmailValidation2");
@@ -87,5 +119,5 @@ public class UserRegistrationGradleTest {
         assertEquals(true, "sampleEmailValidation8");
         assertEquals(true, "sampleEmailValidation9");
     }
-
 }
+
